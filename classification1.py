8d2c8066ec7f0740e69a722cd5813670a6c5d39b
soongjamm <kimstz0303@gmail.com>
2020-09-17 01:32:31 +0900

    분류1

--- classification1.py
+++ classification1.py
@@ -9,15 +9,19 @@ x_data = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]).reshape(10, 1)
 t_data = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1]).reshape(10, 1)
 
 ''  # 2. 임의의 직선 z = Wx + b 정의 (임의의 값으로 가중치 W, 바이어스 b 초기화)
-W = np.random.rand(1, 1)
-b = np.random.rand(1)
+W = np.random.rand(1, 1)  # 배열안에 배열로 된 원소가 하나. (1x1)
+b = np.random.rand(1)  # 1차원 배열, 1개
 print("W = ", W, ", W.shape = ", W.shape, ", b : ", b, ", b.shape = ", b.shape)
 
 
 ''  # 3. 손실함수 E(W,b) 정의
+# 최종출력은 y = sigmoid(Wx+b) 이며, 손실함수는 cross-entropy 로 나타냄
+
 # 임의의 직선값 z가 sigmoid의 인자로 들어가서 판별된다.
 # 그리고 뒤에 과정에서 sigmoid 리턴 값과 정답과 비교한다.
-# 손실함수는 오차를 검증하는 함수라고 보면 된다.
+# 손실함수는 실제값과의 오차를 나타내는 함수라고 보면 된다.
+# 손실함수는 평균제곱 오차와 엔트로피 오차가 있는데, 평균제곱 오차는 회귀 분석에서 사용함.
+# 오차를 가장 작게 만드는게 목표임
 # z = Wx + b, y = sigmoid(z)
 # E(W,b) = -(sigma n, i=1) {t.i log(y.i)log(1-y.i)} --> cross-entropy (공식 유도는 패스..)
 
@@ -34,7 +38,7 @@ def loss_func(x, t):
     y = sigmoid(z)
 
     # cross_entropy
-    return -np.sum(t*np.log(y+delta) + (1-t)*np.log(1-y)+delta)
+    return -np.sum(t*np.log(y+delta) + (1-t)*np.log((1-y)+delta))
 
 
 ''  # 4. 수치미분 numerical_derivative 및 utility 함수 정의
@@ -43,7 +47,7 @@ def loss_func(x, t):
 # sigmoid 값이 0.5 이상이면 1, 이하이면 0
 
 
-def numerical_derivative(f, x):
+def numerical_derivative(f, x):  # f는 함수, x는 W나 b가 온다.
     delta_x = 1e-4  # 0.0001
     grad = np.zeros_like(x)
 
@@ -60,6 +64,7 @@ def numerical_derivative(f, x):
         # 편미분 계산
         x[idx] = tmp_val - delta_x
         fx2 = f(x)  # f(x-delta_x)
+        grad[idx] = (fx1 - fx2) / (2*delta_x)
 
         x[idx] = tmp_val
         it.iternext()
